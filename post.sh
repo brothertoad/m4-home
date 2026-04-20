7bcd9f317d64c7233813531f4fec7eccbfe88e6a
Stacey Brogden <toad@brothertoad.net>
2026-04-20 10:25:36 -0700

    Make post.sh work with dart-sass as well as sassc.

--- post.sh
+++ post.sh
@@ -2,4 +2,9 @@
 
 rsync -a static/ public/
 # sass --no-source-map sass:public
-sassc sass/site.scss public/site.css
+DARTSASS="$HOME/dart-sass/sass"
+if [ -f "$DARTSASS" -a -x "$DARTSASS" ]; then
+    $DARTSASS sass/site.scss public/site.css
+else
+    sassc sass/site.scss public/site.css
+fi
